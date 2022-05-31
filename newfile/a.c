0447ec5f488058fccb892347eab4c7408332736d
Zhao Yi <zhaoyi@uniontech.com>
2022-05-31 16:34:40 +0800

    newfile: Implement fun_add().
    Signed-off-by: Zhao Yi <zhaoyi@uniontech.com>

--- newfile/a.c
+++ newfile/a.c
@@ -1,7 +1,18 @@
 #include <iostream>
 
+int fun_add(int a, int b)
+{
+    printf("a=%d; b=%d\n",a, b);
+    return a+b;
+}
+
 int main()
 {
+    int a=2;
+    int b=3;
+    int c=0;
     printf("hello world!\r\n");
+    c = fun_add(a,b);
+    printf("c=%d\n",c);
     return 0;
 }
